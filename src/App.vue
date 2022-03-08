1286a688aece8cbef53ccd95ff9149bd74b31ca7
1613195189@qq.com <1613195189@qq.com>
2022-03-08 15:59:22 +0800

    顶部布局的实现

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <section class="wrapper">
+    <div class="position">
+      <i class="iconfont position__icon">&#xe619;</i>
+      北京理工大学
+      <i class="iconfont position__notice">&#xe8ae;</i>
+    </div>
+  </section>
   <footer class="tabbar">
     <div class="tabbar__item tabbar__item--active">
       <i class="iconfont">&#xe7c6;</i>
@@ -20,6 +27,41 @@
 </template>
 
 <style lang="scss">
+  @import './style/viriables.scss';
+  @import './style/mixins.scss';
+  .wrapper
+  {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: .49rem;
+    padding: 0 .18rem;
+    background-color: red;
+
+    .position {
+      position: relative;
+      padding: .16rem .24rem .16rem 0;
+      line-height: .22rem;
+      font-size: .16rem;
+      @include ellipsis;
+
+      .position__icon {
+        position: relative;
+        top: .01rem;
+        font-size: .2rem;
+      }
+
+      .position__notice {
+        position: absolute;
+        top: .17rem;
+        right: 0;
+        font-size: .2rem;
+      }
+
+      color: $content-fontcolor;
+    }
+  }
   .tabbar {
     display: flex;
     box-sizing: border-box;
@@ -52,6 +94,8 @@
         color: #1FA4FC;
       }
     }
+
+    color: $content-fontcolor;
   }
 
 </style>
