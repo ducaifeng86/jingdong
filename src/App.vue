0c22165268abaa1bf8be8dc5599f6ec941ab2ae9
1613195189@qq.com <1613195189@qq.com>
2022-03-08 18:03:10 +0800

    附件店铺的布局

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,57 @@
       </div>
     </div>
     <div class="gap"></div>
+    <div class="nearby">
+      <h3 class="nearby__title">附近店铺</h3>
+      <div class="nearby__item">
+        <img class="nearby__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <div class="nearby__item__content">
+          <div class="content__title">沃尔玛</div>
+          <div class="content__tags">
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+          </div>
+          <p class="content_highlight">VIP尊享满80减40</p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img class="nearby__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <div class="nearby__item__content">
+          <div class="content__title">沃尔玛</div>
+          <div class="content__tags">
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+          </div>
+          <p class="content_highlight">VIP尊享满80减40</p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img class="nearby__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <div class="nearby__item__content">
+          <div class="content__title">沃尔玛</div>
+          <div class="content__tags">
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+          </div>
+          <p class="content_highlight">VIP尊享满80减40</p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img class="nearby__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" />
+        <div class="nearby__item__content">
+          <div class="content__title">沃尔玛</div>
+          <div class="content__tags">
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+            <span class="content__tag">月销5万+</span>
+          </div>
+          <p class="content_highlight">VIP尊享满80减40</p>
+        </div>
+      </div>
+    </div>
   </section>
   <footer class="tabbar">
     <div class="tabbar__item tabbar__item--active">
@@ -80,12 +131,13 @@
   @import './style/viriables.scss';
   @import './style/mixins.scss';
   .wrapper {
+    overflow-y:scroll;
     position: absolute;
     left: 0;
     right: 0;
     top: 0;
     bottom: .49rem;
-    padding: 0 .18rem;
+    padding: 0 .18rem 0.13rem .18rem;
 
     .position {
       position: relative;
@@ -161,10 +213,60 @@
       }
     }
 
-    .gap{
-        margin:0 -.18rem;
-        height:.1rem;
-        background-color:#f1f1f1;
+    .gap {
+      margin: 0 -.18rem;
+      height: .1rem;
+      background-color: #f1f1f1;
+    }
+
+    .nearby {
+      &__title {
+        margin: .14rem 0 .02rem 0;
+        font-size: .18rem;
+        font-weight: normal;
+        color: $content-fontcolor;
+      }
+
+      &__item {
+        display: flex;
+        padding-top: .12rem;
+
+        &__img {
+          margin-right: .16rem;
+          width: .56rem;
+          height: .56rem;
+        }
+
+        &__content {
+          flex: 1;
+          padding-bottom: .12rem;
+          border-bottom: 1px solid #f1f1f1;
+
+          .content__title {
+            line-height: .22rem;
+            font-size: .16rem;
+            color: $content-fontcolor;
+          }
+
+          .content__tags {
+            margin-top: .08rem;
+            line-height: .18rem;
+            font-size: .13rem;
+            color: $content-fontcolor;
+
+            .content-tag {
+              margin-right: .16rem;
+            }
+          }
+
+          .content_highlight {
+            margin: .08rem 0 0 0;
+            line-height: .18rem;
+            font-size: .13rem;
+            color: #E93B3B;
+          }
+        }
+      }
     }
   }
   .tabbar {
